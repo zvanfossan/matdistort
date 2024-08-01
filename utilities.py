d2f37d5e9de7fdaaf10a5219647126a6589be017
zach <zvanfossan@gmail.com>
2024-07-31 20:42:54 -0500

    updates

--- utilities.py
+++ utilities.py
@@ -12,8 +12,9 @@ def create_nested_directories(list_of_lists, base_path='base_dir'):
         if not sublists:
             return
         current_level = sublists[0]
-        for i in range(len(current_level)):
-            new_dir_path = os.path.join(current_path, f'dir_{i+1}')
+        for value in current_level:
+            dir_name = str(value).replace('-', 'n')
+            new_dir_path = os.path.join(current_path, dir_name)
             os.makedirs(new_dir_path, exist_ok=True)
             print(f"Directory '{new_dir_path}' created successfully")
             create_dirs(new_dir_path, sublists[1:])
@@ -24,13 +25,19 @@ def create_nested_directories(list_of_lists, base_path='base_dir'):
     
     create_dirs(base_path, list_of_lists)
 
+def create_poscars(parent_structure):
+    """
+    Walk through created directory and 
+    
+    """
+    return
+
+
 # Example usage
 list_of_lists = [
-    [1, 2, 3],  # This will create 3 directories at the first level
+    [-1, 2, 3],  # This will create 3 directories at the first level
     [4, 5],     # This will create 2 directories inside each first level directory
     [6]         # This will create 1 directory inside each second level directory
 ]
 
 create_nested_directories(list_of_lists)
-
-
