7a063bf330fb39d18492c1d2c724e0aebcf2fb67
zach <zvanfossan@gmail.com>
2024-08-07 14:18:08 -0500

    updates

--- matdistort/perovskite.py
+++ matdistort/perovskite.py
@@ -71,81 +71,6 @@ def create_directories_with_poscars(parent_structure, param_dict):
         print(f"Base directory '{base_path}' created successfully")
         create_dirs(base_path, list_of_lists)
 
-    def gamma1():
-        """
-        distort structure according to gm1
-        """
-
-    def gamma3_a0():
-        """
-        distort structure according to gm3_a0
-        """
-    
-    def gamma3_0a():
-        """
-        distort structure according to gm3_0a
-        """
-
-    def gamma5_a00():
-        """
-        distort structure according to gm5_a00
-        """
-
-    def gamma5_0a0():
-        """
-        distort structure according to gm5_0a0
-        """
-
-    def gamma5_00a():
-        """
-        distort structure according to gm5_00a
-        """
-
-    def oct_x():
-        """
-        rotate oxygen octahedra about x axis
-        """
-
-    def oct_y():
-        """
-        rotate oxygen octahedra about y axis
-        """
-
-    def oct_z():
-        """
-        rotate oxygen octahedra about z axis
-        """
-
-    def strain_xx():
-        """
-        apply normal strain along x-axis
-        """
-    
-    def strain_yy():
-        """
-        apply normal strain along y-axis
-        """
-    
-    def strain_zz():
-        """
-        apply normal strain along z-axis
-        """
-    
-    def strain_xy():
-        """
-        apply xy shear strain
-        """
-
-    def strain_xz():
-        """
-        apply xz shear strain
-        """
-
-    def strain_yz():
-        """
-        apply yz shear strain
-        """
-
     def copy_file_to_leaf_directories(file_path, base_path='base_dir'):
         """
         Copies a given file to all leaf directories of the directory structure.
@@ -177,7 +102,7 @@ def create_directories_with_poscars(parent_structure, param_dict):
                 if 'n' in value:
                     directories[i] = value.replace('n', '-')
             float_list = [float(item) for item in directories]
-            ###modify POSCAR structure here and 
+
             lattice = struct.lattice.matrix
             gm1_strain = 0
             gm3_a0_strain = 0
@@ -196,52 +121,48 @@ def create_directories_with_poscars(parent_structure, param_dict):
             eyz = 0
 
             if keys[i] == 'gm1':
-                'math for gm1'
                 gm1_strain = float_list[i]*np.sqrt(3)/3
 
             if keys[i] == 'gm3_a0':
-                'math for gm3_a0'
                 gm3_a0_strain = float_list[i]*np.sqrt(6)/3
 
             if keys[i] == 'gm3_0a':
-                'math for gm3_0a'
                 gm3_0a_strain = float_list[i]*np.sqrt(2)/2
 
             if keys[i] == 'gm5_a00':
-                'math for gm5_a00'
                 gm5_a00_strain = float_list[i]*np.sqrt(2)/2
 
             if keys[i] == 'gm5_0a0':
-                'math for gm5_0a0'
                 gm5_0a0_strain = float_list[i]*np.sqrt(2)/2
 
             if keys[i] == 'gm5_00a':
-                'math for gm5_00a'
                 gm5_00a_strain = float_list[i]*np.sqrt(2)/2
 
             if keys[i] == 'oct_rot_x':
                 'x-rot'
+
             if keys[i] == 'oct_rot_y':
                 'y-rot'
+
             if keys[i] == 'oct_rot_z':
                 'z-rot'
+
             if keys[i] == 'exx':
-                'exx'
                 exx = float_list[i]
+
             if keys[i] == 'eyy':
-                'eyy'
                 eyy = float_list[i]
+
             if keys[i] == 'ezz':
-                'ezz'
                 ezz = float_list[i]
+
             if keys[i] == 'exy':
-                'exy'
                 exy = float_list[i]*np.sqrt(2)/2
+
             if keys[i] == 'exz':
-                'exz'
                 exz = float_list[i]*np.sqrt(2)/2
+
             if keys[i] == 'eyz':
-                'eyz'
                 eyz = float_list[i]*np.sqrt(2)/2
 
             uniaxial_strains = np.zeros((3,3))
